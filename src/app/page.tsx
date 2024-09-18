7c0fad60b44bd4531c1cc669987023c16f27b38a
Jason <jasondanielclarke@gmail.com>
2024-09-18 15:47:23 +0100

    check navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -95,6 +96,9 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <Link href='/' >Home</Link>
+        <Link href='/route1' >Route 1</Link>
+        <Link href='/subroute'> Sublink </Link>
       </footer>
     </div>
   );
