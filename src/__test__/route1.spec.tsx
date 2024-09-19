c539b1169a919ac5f209c2485a4ffa47f5f9bc51
Jason <jasondanielclarke@gmail.com>
2024-09-19 19:41:12 +0100

    add jest to setup

---
+++ src/__test__/route1.spec.tsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '../app/route1/page'
+ 
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Page />)
+ 
+    const deployNow = screen.getByText('Deploy now')
+ 
+    expect(deployNow).toBeInTheDocument()
+  })
+})
